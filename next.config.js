dccd2b58281e067c3c170c2524ca8d016b28ffd5
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-06-25 15:34:34 -0500

    integrated sentry for error management

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ await import("./src/env.js");
 // we will set up github actions to do the type 
 // checking and linting
 /** @type {import("next").NextConfig} */
-const config = {
+const coreConfig = {
 //     Error: Invalid src prop (https://utfs.io/f/004d042b-9e07-48bd-8fb8-0844ab34af97-au325r.jpg) on `next/image`, hostname "utfs.io" is not configured under images in your `next.config.js`
 // See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host
     images: {
@@ -22,5 +22,45 @@ const config = {
         ignoreDuringBuilds: true,
     },
 };
+
+import { withSentryConfig } from "@sentry/nextjs";
+
+const config = withSentryConfig(
+  coreConfig, 
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    org: "nhicaponte",
+    project: "gen-proj",
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    // tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  },
+);
 
 export default config;
