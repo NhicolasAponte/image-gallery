30f9f6dc3443c8c3f529b057010ffa90c974c58d
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-06-26 14:56:00 -0500

    stubbed in component for full page image - need styling

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -6,5 +6,14 @@ export default async function FullPageImageView({
   imageId: number;
 }) {
   const image = await getImageById(imageId);
-  return <img src={image.url} alt={image.name} width={250} />;
+  return (
+    <div className="flex h-full w-full items-center">
+      <div className="flex-shrink border-r">
+        <img src={image.url} alt={image.name} className="object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col bg-black">
+        <h1 className="text-xl font-bold text-white">{image.name}</h1>
+      </div>
+    </div>
+  );
 }
