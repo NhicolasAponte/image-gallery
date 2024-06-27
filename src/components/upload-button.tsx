abd3edc58047a93a4ad5b59025ce48792e4f9a2a
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-06-27 09:51:24 -0500

    using sonner from shadcn - created upload toast - added spinner svg for upload toast

--- src/components/upload-button.tsx
+++ src/components/upload-button.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import { useUploadThing } from "~/utils/uploadthing";
 
 // inferred input off useUploadThing
@@ -46,32 +47,60 @@ function UploadIcon() {
   );
 }
 
-// function LoadingIcon() {
-//   return (
-//     <svg
-//       width="24"
-//       height="24"
-//       viewBox="0 0 24 24"
-//       xmlns="http://www.w3.org/2000/svg"
-//       fill="white"
-//     >
-//       <path
-//         d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
-//         opacity=".25"
-//       />
-//       <path
-//         d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
-//         className="spinner_ajPY"
-//       />
-//     </svg>
-//   );
+function LoadingSpinnerIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+    >
+      <path
+        d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+        opacity=".25"
+      />
+      <path
+        d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+        className="spinner_ajPY"
+      />
+    </svg>
+  );
+}
+// NOTE: this is for testing and debugging ephemeral components
+// function makeUploadToast(){
+//   return toast(
+//     <div className="flex gap-2 items-center ">
+//       <LoadingSpinnerIcon />
+//       <span className="">Uploading...</span>
+//     </div>,
+//     {
+//       duration: 100000,
+//       id: "test-toast"
+//     }
+//   )
 // }
+// window.makeToast = makeUploadToast;
 
 export default function CustomUploadButton() {
   const router = useRouter();
+  // if we expect concurrent uploads, each with their own toast, we need to setup a
+  // derived unique id for each toast
+  const upLoadToastId = "uploading";
 
   const { inputProps } = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast.loading(
+      <div className="flex gap-2 items-center ">
+        <LoadingSpinnerIcon />
+        <span className="">Uploading...</span>
+      </div>, { id: upLoadToastId });
+      //toast("Uploading...", { type: "loading" });
+      //toast("Uploading...");
+      //NOTE: look into sonner api to see what else we can do with the toast
+    },
     onClientUploadComplete() {
+      toast.success("Upload complete", { id: upLoadToastId });
       router.refresh();
     },
   });
