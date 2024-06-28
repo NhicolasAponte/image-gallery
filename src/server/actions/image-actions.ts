59afb8de6836f9ae38efa4b772660727248f8117
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-06-28 10:12:56 -0500

    added server analytics client - captured analytics on delete

--- src/server/actions/image-actions.ts
+++ src/server/actions/image-actions.ts
@@ -4,15 +4,28 @@ import { eq, sql } from "drizzle-orm";
 import { images } from "../db/schema";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import analyticsServerClient from "../analytics";
+import { auth } from "@clerk/nextjs/server";
 
 export async function deleteImageById(id: number): Promise<void> {
-  console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-  console.log(`Deleting image with id ${id}`);
-
+  // console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+  // console.log(`Deleting image with id ${id}`);
+  const user = auth();
+  if (!user.userId) {
+    throw new Error("Not authenticated");
+  }
+  
   await db
     .delete(images)
     .where(eq(images.id, id));
 
+  analyticsServerClient.capture({
+    distinctId: "server",
+    event: "image_deleted",
+    properties: {
+      imageId: id
+    }
+  })
 
   console.log(`Deleted image with id ${id}`);
 
